1a17088daf9280839fb7b363e348c0fcebabefe1
thomcchester <thomcchester@gmail.com>
2016-12-27 16:31:13 -0600

    opps fix

--- makerSetterNode/fish/hold/client/scripts/controllers/logincontrol.js
+++
@@ -1,7 +0,0 @@
-chickAppLogin.controller('LoginController',  ['$scope', '$log', '$http', '$window', function($scope, $log, $http, $window) {
-  //Independent Variables
-  $scope.username_login;
-  $scope.password_login;
-
-
-}]);
